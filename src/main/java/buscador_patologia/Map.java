51856d5537292400e9f9026c60a3c1629fb85673
iulianang <iulian.anghel19@gmail.com>
2017-02-07 21:15:47 +0100

    v2

--- src/main/java/buscador_patologia/Map.java
+++ src/main/java/buscador_patologia/Map.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class Map {
 	
 	public static void main(String[] args) throws Throwable {
+		long startTime = System.currentTimeMillis();
 		
 		Conexion.conexion(Consulta.BUSCAR_PATOLOGIAS_POR_ID);
 //		PatologiasDTO id = new PatologiasDTO();
@@ -13,6 +14,9 @@ public class Map {
 //		HashMap<Integer, String> hmap = new HashMap<Integer, String>();
 //		hmap.put(arg0, arg1);
 		
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println(totalTime);
 	}
 	
 
